c3610643b48ede2a391a1311e095b9f52d5d0940
iameru <post@i3o.eu>
2022-12-22 11:16:52 +0100

    added logo and navbar

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,14 +1,23 @@
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import { navRoutes } from "../routes"
 
 export function Header() {
 
   return (
-  <header className="">
-    <nav>
+  <header className="h-16 p-2 flex justify-around items-center bg-emerald-50">
+    <Link to='/' className="h-full" >
+      <img src="/src/assets/devLogo.svg" className="h-full" />
+    </Link>
+    <nav className="flex gap-4">
       {navRoutes.map(route=>
         <NavLink to={route.path} 
-              key={route.name}>
+              key={route.name}
+              className={ ({isActive})=> {
+                const cls = 'text-2xl uppercase font-medium text-gray-900'
+                return isActive? cls+' hover:cursor-default' : cls+' text-gray-600 hover:text-teal-800'
+               }
+              }
+        >
           {route.name}
         </NavLink>
         )}
