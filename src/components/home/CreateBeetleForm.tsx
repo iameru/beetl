ee17c7705fde62593dab7abb6ca3944e813213d7
iameru <post@i3o.eu>
2022-12-22 22:13:34 +0100

    button navigate

--- src/components/home/CreateBeetleForm.tsx
+++ src/components/home/CreateBeetleForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { getUUID } from "../../api"
 import CurrencyInput from "react-currency-input-field"
+import { useNavigate } from "react-router-dom"
 
 
 type CreateFormState = {
@@ -16,7 +17,17 @@ const defaultFormState:CreateFormState = {
 export function CreateBeetl() {
 
   const [beetl, setBeetl] = useState(defaultFormState)
+  let navigate = useNavigate()
 
+  function handleForm() {
+
+    // build beetlObj to send to API. build route for
+    // button to navigate to it later
+    return {
+      route: `/${beetl.name}/${beetl.targetSum}`,
+      beetlObj: beetl
+    }
+  }
 
   return (
   <form onSubmit={(e)=>e.preventDefault()} 
@@ -27,6 +38,8 @@ export function CreateBeetl() {
         <p className="text-gray-500 text-sm text-right">https://beetl.xyz/</p>
         <input
           value={beetl.name}
+          required={true}
+          maxLength={64}
           placeholder='wie soll der link aussehen'
           className="bg-transparent w-full text-cyan-800" 
           type="text" 
@@ -41,21 +54,29 @@ export function CreateBeetl() {
           <CurrencyInput
             placeholder='0 €'
             suffix=" €"
+            required={true}
             intlConfig={{locale:'de-DE', currency:'EUR'}}
             className="bg-transparent w-full text-cyan-800" 
             allowNegativeValue={false}
             allowDecimals={false}
+            maxLength={64}
             defaultValue={beetl.targetSum}
             onValueChange={(value, name)=>setBeetl( ({
               ...beetl,
               targetSum: parseInt(value as string)
             }))}
             />
-
         </div>
       </div>
       <button type="submit"
         className="py-2 px-4 rounded-r-lg bg-emerald-500 hover:bg-emerald-300"
+        onClick={(e)=>{
+          const {route, beetlObj} = handleForm()
+          // await makeEntryinDB(beetlObj)
+          // if successfull returned 
+          // then(navigate(route))
+          navigate(route)
+        }}
       >
         GO!
       </button>
