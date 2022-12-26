9d098d9263d2ffee81d58dba7865c5ccafbb1390
iameru <post@i3o.eu>
2022-12-26 14:38:48 +0100

    removed mockfunction

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,9 +4,3 @@ export function getUUID () {
   // later get ID from api, for now a mockup
   return crypto.randomUUID().slice(0,8)
 }
-
-
-export async function apiCreateBeetl (data) {
-  // await setTimeout(()=>{},400)
-  return {status: "successfull"}
-}
