fdce12c2065c043cbafd0f452e7f0b3e8a16fb46
iameru <post@i3o.eu>
2022-12-31 15:27:59 +0100

    added error msg field

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { ErrorModal } from "./ErrorModal";
 import { Header } from "./Header";
@@ -6,13 +6,13 @@ import { Header } from "./Header";
   
 export function Layout() {
 
-  const [error, setError] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
 
   const location = useLocation()
-  if (location.state && location.state.error && !error) {
-    // setting error hard for now as not figured out yet
-    setError('Da lief was schief... bitte versuchs nochmal')
-  }
+
+  useEffect( () => {
+    if (location.state?.error) { setErrorMsg(location.state.error) }
+  },[location])
 
   return (
     <div className="w-screen min-h-screen flex flex-col bg-[#f9fbfb]">
@@ -20,7 +20,7 @@ export function Layout() {
       <main className="h-full w-full p-4 flex flex-col gap-6">
         <Outlet />
       </main>
-      {error && <ErrorModal message={error} />}
+      {errorMsg && <ErrorModal message={errorMsg} setMessage={setErrorMsg} />}
     </div>
   )
 }
--- src/layouts/ErrorModal.tsx
+++ src/layouts/ErrorModal.tsx
@@ -1,10 +1,21 @@
 
-export function ErrorModal({message}:{message:string}) {
+type props = {
+  message:string
+  setMessage: React.Dispatch<React.SetStateAction<string>>
+}
+
+export function ErrorModal({message, setMessage}: props) {
 
   return (
-    
+
     <div className="mt-5 p-4 bg-yellow-100 text-center">
       {message}
+      <button
+        onClick={(e)=>{ setMessage('') }}
+        className="border px-1 bg-red-50 hover:bg-emerald-100"
+        >
+        okay
+      </button>
     </div>
   )
 }
