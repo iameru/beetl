f2ba8bfd10629262647e55e24a203ac859d2c08b
iameru <post@i3o.eu>
2023-02-23 12:34:21 +0100

    added tanstack query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,14 +10,18 @@ import LocaleSwitch from "@/components/localeSwitch";
 
 import { Work_Sans } from "@next/font/google";
 
-import {i18n as i18nConfig} from "next-i18next.config";
+import { i18n as i18nConfig } from "next-i18next.config";
 import appConfig from "beetl.config";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const workSans = Work_Sans({ subsets: ["latin"] });
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
 
   return (
     <>
@@ -28,30 +32,38 @@ function App({ Component, pageProps }: AppProps) {
       `}</style>
       <Head>
         <title>beetl</title>
-        <meta name="description" content="equalize finances" />
+        <meta name="description" content="equalize finances, bid together" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         {i18nConfig.locales.map((locale) => (
-        <link key={locale} rel="alternate" hrefLang={locale} href={`${appConfig.domainName}/${locale}${router.asPath}`} />
+          <link
+            key={locale}
+            rel="alternate"
+            hrefLang={locale}
+            href={`${appConfig.domainName}/${locale}${router.asPath}`}
+          />
         ))}
       </Head>
       <div className="px-8 py-5 h-screen w-screen">
-        <div className="flex flex-col gap-4 w-full min-h-full justify-between">
-          <header className="w-full flex justify-between">
-            <Link href="/">Beetl</Link>
-            <LocaleSwitch />
-          </header>
-          <Component {...pageProps} />
-          <footer className="w-full">
-            {router.pathname === "/about" ? (
-              <Link href="/" className="flex items-center">
-                <ChevronLeftIcon className="h-8" /> Create a Beetl
-              </Link>
-            ) : (
-              <Link href="/about">About</Link>
-            )}
-          </footer>
-        </div>
+        <QueryClientProvider client={queryClient}>
+          {appConfig.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+          <div className="flex flex-col gap-4 w-full min-h-full justify-between">
+            <header className="w-full flex justify-between">
+              <Link href="/">Beetl</Link>
+              <LocaleSwitch />
+            </header>
+            <Component {...pageProps} />
+            <footer className="w-full">
+              {router.pathname === "/about" ? (
+                <Link href="/" className="flex items-center">
+                  <ChevronLeftIcon className="h-8" /> Create a Beetl
+                </Link>
+              ) : (
+                <Link href="/about">About</Link>
+              )}
+            </footer>
+          </div>
+        </QueryClientProvider>
       </div>
     </>
   );
