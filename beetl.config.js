474834edbc3eb1e5cccef68500d0cd51e1169a7e
iameru <post@i3o.eu>
2023-02-23 12:32:52 +0100

    added reading from env

--- beetl.config.js
+++ beetl.config.js
@@ -1,4 +1,5 @@
 const appConfig = {
-  domainName: "https://beetl.xyz",
+  domainName: process.env.NEXT_PUBLIC_DOMAIN_NAME, 
+  DEV: process.env.NEXT_PUBLIC_DEVELOPMENTMODE
 };
 export default appConfig;
