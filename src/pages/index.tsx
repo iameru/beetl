56cdf3dfc182e4027d30ca8511aabfb70c3d8cba
iameru <post@i3o.eu>
2023-02-23 11:47:46 +0100

    added custom color palette

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,23 @@
-import {useTranslation} from "next-i18next";
+import { useTranslation } from "next-i18next";
 import { GetStaticProps } from "next";
-import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-type Props = {}
+type Props = {};
 
 export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => ({
   props: {
-    ...(await serverSideTranslations(locale ?? 'de', ["home",'common'])),
+    ...(await serverSideTranslations(locale ?? "de", ["home", "common"])),
   },
 });
 
-export default function Index(props:Props) {
+export default function Index(props: Props) {
+  const { t } = useTranslation();
 
-  const {t} = useTranslation()
-
-  return <div className="flex-1">
-    <p>Home</p>
-    <p>{t("common:appname")} von common</p>
-    <h2 className="bg-yellow-50 p-10 text-4xl">{t("home:testFromHome")}</h2>
-  </div>;
+  return (
+    <div className="flex-1">
+      <p>Home</p>
+      <p>{t("common:appname")} von common</p>
+      <h2 className="bg-yellow-50 p-10 text-4xl">{t("home:testFromHome")}</h2>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#2a466b",
+          light: "#6a7e97",
+          dark: "#1d314b"
+        },
+        secondary: {
+          DEFAULT: "#abacae",
+          light: "#f4f6f8",
+          dark: "#78787a"
+        },
+        signal: {
+          min: '#597936',
+          mid: '#007c82',
+          max: '#385887',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
