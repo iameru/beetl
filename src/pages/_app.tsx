bff427b90573e59a691fe2e12994e3e5e7938bbe
iameru <post@i3o.eu>
2023-02-23 11:21:27 +0100

    added locale alternatives to header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,10 +10,14 @@ import LocaleSwitch from "@/components/localeSwitch";
 
 import { Work_Sans } from "@next/font/google";
 
+import {i18n as i18nConfig} from "next-i18next.config";
+import appConfig from "beetl.config";
+
 const workSans = Work_Sans({ subsets: ["latin"] });
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
 
   return (
     <>
@@ -27,6 +31,9 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="equalize finances" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        {i18nConfig.locales.map((locale) => (
+        <link key={locale} rel="alternate" hrefLang={locale} href={`${appConfig.domainName}/${locale}${router.asPath}`} />
+        ))}
       </Head>
       <div className="px-8 py-5 h-screen w-screen">
         <div className="flex flex-col gap-4 w-full min-h-full justify-between">
